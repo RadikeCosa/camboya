2f2cf67e47c8b459dc6c638e2973bc58f3711955
Radike Cosa <ramirocosa@gmail.com>
2025-12-01 18:36:23 -0300

    link agregado

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div>
       <LinkButton href="/ESAS">Go ESAS Assessment Tool</LinkButton>
+      <LinkButton href="/data-viz">Go to LeetCode Problems Viz</LinkButton>
     </div>
   );
 }
